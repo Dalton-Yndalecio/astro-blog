c10ae7360ae88d0f97873d95c6cd77e3e7e8782f
DALTON <yndaleciorodriguezd@gmail.com>
2025-09-28 12:04:02 -0500

    agregar soporte para Markdown y sanitización de HTML en la generación de RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,13 @@
 import type { APIRoute } from 'astro'
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
 
-export const GET: APIRoute = async  ({ params, request, site }) => {
-	const blogPosts = await getCollection('blog');
+const parser = new MarkdownIt()
+
+export const GET: APIRoute = async ({ params, request, site }) => {
+	const blogPosts = await getCollection('blog')
 
 	return rss({
 		// stylesheet: '/styles/rss.xsl',
@@ -11,16 +15,30 @@ export const GET: APIRoute = async  ({ params, request, site }) => {
 		title: 'Dalton Blog',
 		// `<description>` field in output xml
 		description: 'Un simple blog sobre mi',
+		xmlns: {
+			media: 'http://search.yahoo.com/mrss/'
+		},
 		// Pull in your project "site" from the endpoint context
 		// https://docs.astro.build/en/reference/api-reference/#site
 		site: site!,
 		// Array of `<item>`s in output xml
 		// See "Generating items" section for examples using content collections and glob imports
-		items: blogPosts.map(({ data, slug }) => ({
+		items: blogPosts.map(({ data, slug, body }) => ({
 			title: data.title,
 			pubDate: data.date,
 			description: data.description,
-			link: `/posts/${slug}`
+			link: `/posts/${slug}`,
+			content: sanitizeHtml(parser.render(body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+			}),
+
+			customData: `<media:content
+    type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+    width="${data.image.width}"
+    height="${data.image.height}"
+    medium="image"
+    url="${site + data.image.src}" />
+`
 		})),
 		// (optional) inject custom xml
 		customData: `<language>es-mx</language>`
